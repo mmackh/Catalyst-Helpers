72743658e805dbb2e873d4757daabe4e2b254817
Maximilian Mackh <mmackh@gmail.com>
2019-10-13 17:28:30 +0200

    Add files via upload

---
+++ IPDFToolbarHelper.h
@@ -0,0 +1,30 @@
+//
+//  IPDFToolbarHelper.h
+//  InstaPDF for Mac
+//
+//  Created by mmackh on 13.10.19.
+//  Copyright © 2019 mackh ag. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface NSToolbarItem_Catalyst : NSToolbarItem
+
+@property id view;
+
+@property CGSize minSize;
+@property CGSize maxSize;
+
++ (instancetype)searchItemWithItemIdentifier:(NSString *)itemIdentifier textDidChangeHandler:(void(^)(NSString *stringValue))textDidChangeHandler;
+
+@property (nonatomic) NSString *searchFieldStringValue;
+
+@end
+
+@interface IPDFToolbarHelper : NSObject
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ IPDFToolbarHelper.m
@@ -0,0 +1,88 @@
+//
+//  IPDFToolbarHelper.m
+//  InstaPDF for Mac
+//
+//  Created by mmackh on 13.10.19.
+//  Copyright © 2019 mackh ag. All rights reserved.
+//
+
+#import "IPDFToolbarHelper.h"
+
+#import <UIKit/NSToolbar+UIKitAdditions.h>
+
+@class NSSearchField_Catalyst;
+
+@protocol NSSearchFieldDelegate_Catalyst
+
+@optional
+- (void)searchFieldDidStartSearching:(NSSearchField_Catalyst *)sender;
+- (void)searchFieldDidEndSearching:(NSSearchField_Catalyst *)sender;
+
+@required
+- (void)controlTextDidChange:(NSNotification *)notification;
+
+@end
+
+@interface NSSearchField_Catalyst : NSObject
+
+@property(weak) id<NSSearchFieldDelegate_Catalyst> delegate;
+
+@property (nonatomic,readwrite) NSString *stringValue;
+
+@end
+
+@interface NSToolbarItem_Catalyst () <NSSearchFieldDelegate_Catalyst>
+
+@property (nonatomic,copy) void(^textDidChangeHandler)(NSString *stringValue);
+
+@property id image;
+@property id imageStore;
+
+@end
+
+@implementation NSToolbarItem_Catalyst
+
+@dynamic view;
+@dynamic minSize;
+@dynamic maxSize;
+
+@dynamic image;
+
++ (instancetype)searchItemWithItemIdentifier:(NSString *)itemIdentifier textDidChangeHandler:(void(^)(NSString *stringValue))textDidChangeHandler
+{
+    NSToolbarItem_Catalyst *toolbarItem = [[[self class] alloc] initWithItemIdentifier:itemIdentifier];
+    
+    NSSearchField_Catalyst *searchField = [NSClassFromString(@"NSSearchField") new];
+    searchField.delegate = toolbarItem;
+    toolbarItem.view = searchField;
+    toolbarItem.maxSize = CGSizeMake(240, 44);
+    toolbarItem.textDidChangeHandler = textDidChangeHandler;
+    toolbarItem.paletteLabel = @"Search";
+    return toolbarItem;
+}
+
+- (void)controlTextDidChange:(NSNotification *)notification
+{
+    NSSearchField_Catalyst *searchField = notification.object;
+    if (self.textDidChangeHandler) self.textDidChangeHandler(searchField.stringValue);
+}
+
+- (void)setSearchFieldStringValue:(NSString *)searchFieldStringValue
+{
+    if (![self.view isKindOfClass:NSClassFromString(@"NSSearchField")]) return;
+    NSSearchField_Catalyst *searchField = self.view;
+    searchField.stringValue = searchFieldStringValue;
+}
+
+- (NSString *)searchFieldStringValue
+{
+    if (![self.view isKindOfClass:NSClassFromString(@"NSSearchField")]) return @"";
+    NSSearchField_Catalyst *searchField = self.view;
+    return searchField.stringValue;
+}
+
+@end
+
+@implementation IPDFToolbarHelper
+
+@end
